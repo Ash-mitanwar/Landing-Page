5625af0e282a9e3aca4971eaf50bea93606964cd
Ash-mitanwar <ashmitharyana@gmail.com>
2025-05-21 08:51:52 +0530

    Wolves

---
+++ script.js
@@ -0,0 +1,16 @@
+// Reveal on scroll
+const sections = document.querySelectorAll('section');
+
+// function revealSections() {
+//   const triggerBottom = window.innerHeight * 0.85;
+
+//   sections.forEach(section => {
+//     const sectionTop = section.getBoundingClientRect().top;
+//     if (sectionTop < triggerBottom) {
+//       section.classList.add('visible');
+//     }
+//   });
+// }
+
+// window.addEventListener('scroll', revealSections);
+// window.addEventListener('load', revealSections);
